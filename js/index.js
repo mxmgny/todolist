49721a551ef7004510e85896e464a17f8fc28727
Maksym Hnoinskyi <mxmgny@vivaldi.net>
2019-05-05 13:25:17 +0300

    Print tasks in todo list

--- js/index.js
+++ js/index.js
@@ -1,35 +1,43 @@
 window.onload  = function(){
-let tasks = [];
-if( window.localStorage.getItem( 'todo-list' ) != null ) {
-        tasks = JSON.parse(window.localStorage.getItem('todo-list'));
+    let tasks = [];
+    if( window.localStorage.getItem( 'todo-list' ) != null ) {
+            tasks = JSON.parse(window.localStorage.getItem('todo-list'));
+            printTasks();
+        }
+    document.getElementById('reset').onclick = function(){ 
+        tasks = [];
+        window.localStorage.removeItem('todo-list'); 
+        printTasks()
+    };
+    function getTask() {
+        
+        let user = document.getElementById('user').value;
+        let date = document.getElementById('date').value;
+        let task = document.getElementById('task').value;
+        if(user && date && task) {
+            let tempTask ={};
+            tempTask.name = user;
+            tempTask.date = date;
+            tempTask.task = task;
+            tempTask.status = "waiting";
+            tempTask.edited = false;
+            tasks.push(tempTask);
+            window.localStorage.setItem('todo-list',JSON.stringify(tasks) );
+        }
     }
-document.getElementById('reset').onclick = function(){ 
-    tasks = [];
-    window.localStorage.removeItem('todo-list'); 
-    console.log(tasks, window.localStorage.getItem('todo-list'));
-};
-function getTask() {
     
-    let user = document.getElementById('user').value;
-    let date = document.getElementById('date').value;
-    let task = document.getElementById('task').value;
-    if(user && date && task) {
-        let tempTask ={};
-        tempTask.name = user;
-        tempTask.date = date;
-        tempTask.task = task;
-        tempTask.status = "waiting";
-        tempTask.edited = false;
-        tasks.push(tempTask);
-        window.localStorage.setItem('todo-list',JSON.stringify(tasks) );
+    function printTasks() {
+        let out = '';
+        for(let key in tasks){
+            out += `<li>${tasks[key].name} | ${tasks[key].date} | ${tasks[key].task}</li>`;
+        }
+        document.getElementById('task-list').innerHTML =out;
     }
-    console.log(window.localStorage.getItem('todo-list'), tasks);
-}
-
-
-document.getElementById('submit').addEventListener('click', function(){
     
-    getTask();
-    console.log(tasks);
-});
-}
+    
+    document.getElementById('submit').addEventListener('click', function(){
+        
+        getTask();
+        printTasks();
+    });
+    }
